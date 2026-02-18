d683853d144f32efb8963ac19591c285190833f8
Abhay Golande <golandeabhay947@gmail.com>
2026-02-18 17:39:53 +0530

    fixed render port issue

--- app.js
+++ app.js
@@ -137,6 +137,9 @@ app.use((err, req, res, next) => {
 // ============================
 // Server
 // ============================
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
